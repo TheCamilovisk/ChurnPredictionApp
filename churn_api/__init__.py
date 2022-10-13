00230f7a99fbbc385873a3b7b8be48eec358ea31
TheCamilovisk <camilolgon@gmail.com>
2022-10-13 18:26:42 -0300

    Add CORS and features router.

--- churn_api/__init__.py
+++ churn_api/__init__.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-import churn_api.routers.predict as predict
+from churn_api.routers import predict, features
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["POST", "GET"],
+)
 
 app.include_router(predict.router)
+app.include_router(features.router)
 
 
 @app.get("/")
