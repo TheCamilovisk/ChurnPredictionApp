8d655f8a87382c20b9cb879c69347521e80365f5
TheCamilovisk <camilolgon@gmail.com>
2022-10-13 18:26:04 -0300

    Adding the features router.

---
+++ churn_api/routers/features.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+from .. import models
+
+
+router = APIRouter(prefix="/features", tags=["features"])
+
+
+@router.get("/categorical")
+async def get_categorical_features_values() -> dict:
+    feature_values = {
+        "MultipleLines": [e.value for e in models.PhoneDependentService],
+        "InternetService": [e.value for e in models.InternetService],
+        "OnlineSecurity": [e.value for e in models.InternetDependentService],
+        "OnlineBackup": [e.value for e in models.InternetDependentService],
+        "DeviceProtection": [e.value for e in models.InternetDependentService],
+        "TechSupport": [e.value for e in models.InternetDependentService],
+        "StreamingTV": [e.value for e in models.InternetDependentService],
+        "StreamingMovies": [e.value for e in models.InternetDependentService],
+        "Contract": [e.value for e in models.Contract],
+        "PaymentMethod": [e.value for e in models.PaymentMethod],
+    }
+
+    return {"categorical_feature_values": feature_values}
