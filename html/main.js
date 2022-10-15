a1d134ca27b50f1f4259c598409b7db0555d45aa
TheCamilovisk <camilolgon@gmail.com>
2022-10-15 01:39:25 -0300

    Function front-end script.

--- html/main.js
+++ html/main.js
@@ -1,14 +1,29 @@
-const url = "/api/features/categorical";
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    const categoricalValues = data["categorical_feature_values"];
-    Object.entries(categoricalValues).forEach(([elementId, optionsList]) => {
-      addOptions(elementId, optionsList);
-    });
+const onPageLoad = () => {
+  const url = "http://localhost:8000/features";
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const categoricalValues = data["categorical_feature_values"];
+      Object.entries(categoricalValues).forEach(([elementId, optionsList]) => {
+        addOptions(elementId, optionsList);
+      });
 
-    checkFields();
+      checkFields();
+    });
+  console.log("Paged loaded!");
+
+  const form = document.getElementById("form");
+  form.addEventListener("submit", (event) => {
+    console.log("Submitted!");
+    cleanErrors();
+    const gender = document.querySelector('input[name="gender"]:checked');
+    if (!gender) showError();
+    else callPredictionApi();
+
+    event.preventDefault();
   });
+  console.log("AKI1");
+};
 
 const addOptions = (elementId, optionsList) => {
   const selectElement = document.getElementById(elementId);
@@ -64,3 +79,83 @@ const handleServiceStateChange = (serviceType, isServiceEnabled) => {
       }
     });
 };
+
+const getGender = () => {
+  document.querySelector('input[name="gender"]:checked').value;
+};
+
+const getFeatureValue = (featureName) => {
+  const featureTag = document.getElementsByName(featureName)[0];
+  let value = null;
+  if (featureTag.tagName.toLowerCase() == "input") {
+    switch (featureTag.type.toLowerCase()) {
+      case "checkbox":
+        value = featureTag.value == "on" ? "Yes" : "No";
+        break;
+      case "text":
+        value = parseInt(featureTag.value);
+        break;
+      default:
+        value = featureTag.value;
+        break;
+    }
+  } else value = featureTag.value;
+  return value;
+};
+
+const getFeatures = () => {
+  const gender = document.querySelector('input[name="gender"]:checked');
+
+  const data = {
+    gender: gender.value,
+    SeniorCitizen: getFeatureValue("SeniorCitizen"),
+    Partner: getFeatureValue("Partner"),
+    Dependents: getFeatureValue("Dependents"),
+    PhoneService: getFeatureValue("PhoneService"),
+    MultipleLines: getFeatureValue("MultipleLines"),
+    InternetService: getFeatureValue("InternetService"),
+    OnlineSecurity: getFeatureValue("OnlineSecurity"),
+    OnlineBackup: getFeatureValue("OnlineBackup"),
+    DeviceProtection: getFeatureValue("DeviceProtection"),
+    TechSupport: getFeatureValue("TechSupport"),
+    StreamingTV: getFeatureValue("StreamingTV"),
+    StreamingMovies: getFeatureValue("StreamingMovies"),
+    Contract: getFeatureValue("Contract"),
+    PaperlessBilling: getFeatureValue("PaperlessBilling"),
+    PaymentMethod: getFeatureValue("PaymentMethod"),
+    tenure: getFeatureValue("tenure"),
+    MonthlyCharges: getFeatureValue("MonthlyCharges"),
+    TotalCharges: getFeatureValue("TotalCharges"),
+  };
+  return data;
+};
+
+const cleanErrors = () => {
+  const genderErrorTag = document.getElementById("error");
+  genderErrorTag.innerText = "";
+};
+
+const showError = () => {
+  const genderErrorTag = document.getElementById("error");
+  genderErrorTag.innerText = "Required";
+};
+
+const callPredictionApi = async () => {
+  const url = "http://localhost:8000/predict";
+  const data = [getFeatures()];
+  const prediction = fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => showPrediction(data["predictions"]));
+  return prediction;
+};
+
+const showPrediction = (predictions) => {
+  const predictionTag = document.getElementById("prediction");
+  predictionTag.innerText = predictions[0][null];
+};
+
+window.onload = onPageLoad;
