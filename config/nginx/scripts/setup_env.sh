3e43289afcef2e9c2ca0a2d46320de7bd290e8fe
TheCamilovisk <camilolgon@gmail.com>
2022-10-16 14:10:23 -0300

    Isolate each setup step.

--- config/nginx/scripts/setup_env.sh
+++ config/nginx/scripts/setup_env.sh
@@ -1,9 +1,3 @@
-sudo amazon-linux-extras install -y docker
-sudo amazon-linux-extras install -y nginx1
-
-sudo gpasswd -a ec2-user docker
-sudo gpasswd -a nginx ec2-user
-
 sudo cp /home/ec2-user/ChurnPredictionApp/config/nginx/default.conf /etc/nginx/conf.d/default.conf
 
 sudo chmod -R g+x /home/ec2-user/
@@ -14,4 +8,3 @@ sudo service docker restart
 
 aws ecr get-login-password  --region sa-east-1 | docker login --username AWS --password-stdin 877885770422.dkr.ecr.sa-east-1.amazonaws.com
 docker pull 877885770422.dkr.ecr.sa-east-1.amazonaws.com/churn_prediction_api:latest
-
---
+++ config/nginx/scripts/start.sh
@@ -0,0 +1,1 @@
+docker run -d -p 8000:8000 877885770422.dkr.ecr.sa-east-1.amazonaws.com/churn_prediction_api:latest
