4cb78259c09333b77ac7dd9fabb7e4e39c14e69e
TheCamilovisk <camilolgon@gmail.com>
2022-10-25 21:59:07 -0300

    Modify model_training.py to upload the trained model to the specified S# bucket.

--- api/model_training.py
+++ api/model_training.py
@@ -1,5 +1,8 @@
 import os
+import typing
+from argparse import ArgumentParser
 
+import boto3
 import joblib
 import pandas as pd
 from sklearn.compose import make_column_transformer
@@ -7,11 +10,20 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import LabelBinarizer, MinMaxScaler, OneHotEncoder
 
-if __name__ == "__main__":
+
+def write_model_to_bucket(output_bucket_name: str) -> None:
+    s3_resource = boto3.resource("s3")
+    bucket = s3_resource.Bucket(output_bucket_name)
+    bucket.upload_file(Filename="models/lr_model.joblib", Key="models/lr_model.joblib")
+
+
+def main(output_bucket_name: typing.Optional[str]) -> None:
     churn_df = pd.read_csv("data/WA_Fn-UseC_-Telco-Customer-Churn.csv")
     churn_df.TotalCharges = pd.to_numeric(churn_df.TotalCharges, errors="coerce")
     churn_df.dropna(axis="rows", inplace=True)
-    churn_df.SeniorCitizen = churn_df.SeniorCitizen.apply(lambda val: "Yes" if val else "No")
+    churn_df.SeniorCitizen = churn_df.SeniorCitizen.apply(
+        lambda val: "Yes" if val else "No"
+    )
 
     categorical_features = [
         "gender",
@@ -56,3 +68,21 @@ if __name__ == "__main__":
         os.makedirs("models")
 
     joblib.dump(model_pipeline, "models/lr_model.joblib")
+
+    if output_bucket_name:
+        write_model_to_bucket(output_bucket_name)
+
+
+if __name__ == "__main__":
+    ap = ArgumentParser()
+    ap.add_argument(
+        "-b",
+        "--bucket-name",
+        type=str,
+        required=False,
+        help="S3 bucket where the model will be uploaded to. It assumed that the bucket exists.",
+    )
+    args = ap.parse_args()
+
+    bucket_name = args.bucket_name
+    main(bucket_name)
