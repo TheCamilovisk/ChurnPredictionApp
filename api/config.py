02004a1b8e13443d32b6ec1e8a05f1b730ecbd50
TheCamilovisk <camilolgon@gmail.com>
2022-10-26 21:54:01 -0300

    Adjusting config.py to not throw error if environment variables are not set.

--- api/config.py
+++ api/config.py
@@ -7,5 +7,5 @@ ROOT = path.normpath(path.dirname(__file__))
 
 load_dotenv(path.join(ROOT, ".env"))
 
-BUCKET_NAME = os.environ["BUCKET_NAME"]
-MODEL_ARTIFACT_PATH = os.environ["MODEL_ARTIFACT_PATH"]
+BUCKET_NAME = os.environ.get("BUCKET_NAME")
+MODEL_ARTIFACT_PATH = os.environ.get("MODEL_ARTIFACT_PATH")
