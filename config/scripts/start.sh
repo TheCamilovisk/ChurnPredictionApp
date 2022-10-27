442dd338ed27081d32be68e326fd974441464208
TheCamilovisk <camilolgon@gmail.com>
2022-10-26 22:12:03 -0300

    Adjusting the docker run command to use the required env variables.

--- config/scripts/start.sh
+++ config/scripts/start.sh
@@ -1,1 +1,4 @@
-docker run -d -p 8000:8000 877885770422.dkr.ecr.sa-east-1.amazonaws.com/churn_prediction_api:latest
+docker run -d -p 8000:8000 \
+    -e BUCKET_NAME=$BUCKET_NAME \
+    -e MODEL_ARTIFACT_PATH=$MODEL_ARTIFACT_PATH \
+    877885770422.dkr.ecr.sa-east-1.amazonaws.com/churn_prediction_api:latest
